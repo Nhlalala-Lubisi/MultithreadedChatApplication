08508b18a0786092ac4688641d5de9a80405959c
Nhlalala Lubisi <lubisinhlaks685@gmail.com>
2026-07-03 16:03:12 +0200

    Add assertion for exception in ClientHandlerTest

--- src/test/java/com/nhlaks/ClientHandlerTest.java
+++ src/test/java/com/nhlaks/ClientHandlerTest.java
@@ -2,6 +2,7 @@ package com.nhlaks;
 
 import java.net.Socket;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,9 @@ class ClientHandlerTest {
     @Test
     void shouldThrowExceptionWhenSocketIsNull() {
 
-        assertThrows(NullPointerException.class,
-                () -> new ClientHandler((Socket) null));
+        NullPointerException ex = assertThrows(NullPointerException.class,
+            () -> new ClientHandler((Socket) null));
+        assertNotNull(ex);
 
     }
 
