785e2552be55df5ce04e40177a56f40d94b2cd9a
Nhlalala Lubisi <lubisinhlaks685@gmail.com>
2026-07-03 18:05:32 +0200

    Update ChatServer.java

--- src/main/java/com/nhlaks/ChatServer.java
+++ src/main/java/com/nhlaks/ChatServer.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 import java.util.List;
 
 public class ChatServer {
-
+    // Port number
     public static final int PORT = 5000;
 
     private static final List<ClientHandler> clients = new java.util.concurrent.CopyOnWriteArrayList<>();
@@ -18,13 +18,14 @@ public class ChatServer {
         System.out.println("Waiting for clients...");
 
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-
+            // Server continuously accepts new clients
             while (running && !serverSocket.isClosed()) {
 
                 Socket socket = serverSocket.accept();
 
                 System.out.println("New client connected.");
-
+               
+                // Create a handler for the new client
                 ClientHandler clientHandler = new ClientHandler(socket);
 
                 clients.add(clientHandler);
